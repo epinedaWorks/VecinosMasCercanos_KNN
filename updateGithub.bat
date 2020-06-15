643713c6d560bd870a4a696a782b0bc0fdc6318b
José Pineda <epineda.amezquita@gmail.com>
2020-06-15 16:55:03 -0600

    Primer Commit

---
+++ updateGithub.bat
@@ -0,0 +1,10 @@
+echo "Algoritmo K vecinos mas próximos "
+mensaje=$1
+
+git status
+git add .
+git commit -m $mensaje
+echo Enviando a github...
+sleep 2
+echo ...
+git push -u RegresionPolinomial master
